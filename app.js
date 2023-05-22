8c1c38fba03a093d4f24b2417201f3b5acd84041
nikolai-petukhov <nikolaipetukhov@gmai.com>
2023-05-22 16:32:52 +0300

    fix links to imgs

---
+++ app.js
@@ -0,0 +1,8 @@
+const countDownDate = new Date("May 31, 2023 00:00:00").getTime();
+
+const days = document.getElementById('days')
+const hours = document.getElementById('hours')
+const minutes = document.getElementById('minutes')
+const seconds = document.getElementById('seconds')
+
+console.log(countDownDate);
